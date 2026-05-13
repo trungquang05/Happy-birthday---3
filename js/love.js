3a968771db7f542662664e1c1c4fadac521b5952
trungquang05 <162691275+trungquang05@users.noreply.github.com>
2026-05-13 09:27:43 +0700

    tn4

--- js/love.js
+++ js/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("ấn vào đây nè", 15, 10);
+            ctx.fillText("ấn vào tim nè c", 15, 10);
             ctx.restore();
         },
         clear: function() {
